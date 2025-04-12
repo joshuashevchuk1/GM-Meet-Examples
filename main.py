2ff2e02a8702bfe65e37a25462b6422a0d3f94e7
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-04-12 13:28:28 -0700

    update main.py and requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 import auth
+from google.apps import meet_v2 as meet
+
+def create_space(USER_CREDENTIALS) -> meet.Space:
+    """Create a meeting space."""
+    client = meet.SpacesServiceClient(credentials=USER_CREDENTIALS)
+    request = meet.CreateSpaceRequest()
+    return client.create_space(request=request)
 
 def main():
     USER_CREDENTIALS = auth.get_credentials()
+    create_space(USER_CREDENTIALS)
 
 if __name__ == "__main__":
     main()
