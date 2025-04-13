94958f76a41d7cd766efab26d112b51df33b1e0f
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-04-12 17:36:22 -0700

    trying to debug the pubsub

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def create_space() -> meet.Space:
     request = meet.CreateSpaceRequest()
     return client.create_space(request=request)
 
-def subscribe_to_space(space_name: str = None, topic_name: str = None, USER_CREDENTIALS: str = None):
+def subscribe_to_space(space_name: str = None, topic_name: str = None):
     """Subscribe to events for a meeting space."""
     session = requests.AuthorizedSession(USER_CREDENTIALS)
     body = {
@@ -50,7 +50,7 @@ def format_participant(participant: meet.Participant) -> str:
     return "Unknown participant"
 
 
-def fetch_participant_from_session(session_name: str,USER_CREDENTIALS: str = None) -> meet.Participant:
+def fetch_participant_from_session(session_name: str) -> meet.Participant:
     """Fetches the participant for a session."""
     client = meet.ConferenceRecordsServiceClient(credentials=USER_CREDENTIALS)
     # Use the parent path of the session to fetch the participant details
@@ -61,7 +61,7 @@ def fetch_participant_from_session(session_name: str,USER_CREDENTIALS: str = Non
     return client.get_participant(name=participant_resource_name)
 
 
-def on_conference_started(message: pubsub_v1.subscriber.message.Message,USER_CREDENTIALS: str = None):
+def on_conference_started(message: pubsub_v1.subscriber.message.Message):
     """Display information about a conference when started."""
     payload = json.loads(message.data)
     resource_name = payload.get("conferenceRecord").get("name")
@@ -70,7 +70,7 @@ def on_conference_started(message: pubsub_v1.subscriber.message.Message,USER_CRE
     print(f"Conference (ID {conference.name}) started at {conference.start_time.rfc3339()}")
 
 
-def on_conference_ended(message: pubsub_v1.subscriber.message.Message,USER_CREDENTIALS: str = None):
+def on_conference_ended(message: pubsub_v1.subscriber.message.Message):
     """Display information about a conference when ended."""
     payload = json.loads(message.data)
     resource_name = payload.get("conferenceRecord").get("name")
@@ -79,7 +79,7 @@ def on_conference_ended(message: pubsub_v1.subscriber.message.Message,USER_CREDE
     print(f"Conference (ID {conference.name}) ended at {conference.end_time.rfc3339()}")
 
 
-def on_participant_joined(message: pubsub_v1.subscriber.message.Message,USER_CREDENTIALS: str = None):
+def on_participant_joined(message: pubsub_v1.subscriber.message.Message):
     """Display information about a participant when they join a meeting."""
     payload = json.loads(message.data)
     resource_name = payload.get("participantSession").get("name")
@@ -90,7 +90,7 @@ def on_participant_joined(message: pubsub_v1.subscriber.message.Message,USER_CRE
     print(f"{display_name} joined at {session.start_time.rfc3339()}")
 
 
-def on_participant_left(message: pubsub_v1.subscriber.message.Message,USER_CREDENTIALS: str = None):
+def on_participant_left(message: pubsub_v1.subscriber.message.Message):
     """Display information about a participant when they leave a meeting."""
     payload = json.loads(message.data)
     resource_name = payload.get("participantSession").get("name")
@@ -101,7 +101,7 @@ def on_participant_left(message: pubsub_v1.subscriber.message.Message,USER_CREDE
     print(f"{display_name} left at {session.end_time.rfc3339()}")
 
 
-def on_recording_ready(message: pubsub_v1.subscriber.message.Message,USER_CREDENTIALS: str = None):
+def on_recording_ready(message: pubsub_v1.subscriber.message.Message):
     """Display information about a recorded meeting when artifact is ready."""
     payload = json.loads(message.data)
     resource_name = payload.get("recording").get("name")
@@ -110,7 +110,7 @@ def on_recording_ready(message: pubsub_v1.subscriber.message.Message,USER_CREDEN
     print(f"Recording available at {recording.drive_destination.export_uri}")
 
 
-def on_transcript_ready(message: pubsub_v1.subscriber.message.Message,USER_CREDENTIALS: str = None):
+def on_transcript_ready(message: pubsub_v1.subscriber.message.Message):
     """Display information about a meeting transcript when artifact is ready."""
     payload = json.loads(message.data)
     resource_name = payload.get("transcript").get("name")
@@ -148,10 +148,19 @@ def listen_for_events(subscription_name: str = None):
         print("Listening for events")
         try:
             future.result()
+            print("future.result() :  " , future.result())
         except KeyboardInterrupt:
             future.cancel()
+        except Exception as e:
+            print("got an exception ", str(e))
     print("Done")
 
 USER_CREDENTIALS = auth.get_credentials()
-create_space()
+space = create_space()
+print(f"Join the meeting at {space.meeting_uri}")
 
+TOPIC_NAME = "projects/zd-hackathon-2025/topics/workspace-events"
+SUBSCRIPTION_NAME = "projects/zd-hackathon-2025/subscriptions/workspace-events-sub"
+#
+subscription = subscribe_to_space(topic_name=TOPIC_NAME, space_name=space.name)
+listen_for_events(subscription_name=SUBSCRIPTION_NAME)
