baf355fc1c8057dfd5cbdfea19039df240e7121a
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-04-15 22:40:12 -0700

    got a downloadable transcript working but it takes a wwwwwwhile lol

--- download_transcript_test.py
+++ download_transcript_test.py
@@ -3,16 +3,14 @@ from googleapiclient.discovery import build
 import io
 from googleapiclient.http import MediaIoBaseDownload
 
-# If modifying these SCOPES, delete the token.json file.
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 def download_google_doc(file_id, export_mime='application/pdf', output_file='output.pdf'):
     creds = None
 
-    # Load credentials (or get new ones)
     if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file(
-            'oauth.json', SCOPES)  # You need to download this from Google Cloud
+            'oauth.json', SCOPES)
         creds = flow.run_local_server(port=0)
 
     service = build('drive', 'v3', credentials=creds)
@@ -30,6 +28,5 @@ def download_google_doc(file_id, export_mime='application/pdf', output_file='out
 
     print(f"Downloaded '{output_file}' successfully.")
 
-# Replace with your file's ID
 file_id = '1_CLDXIbUf3b42WNtzcVIEk09wrcc9ILqrfw1gjdmHN4'
 download_google_doc(file_id, export_mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document', output_file='Transcript_test.docx')
