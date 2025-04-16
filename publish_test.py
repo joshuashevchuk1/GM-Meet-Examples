702a039f9f6e97025135bb5ccfdc22073cd43430
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-04-15 21:49:46 -0700

    added a publish_test

---
+++ publish_test.py
@@ -0,0 +1,10 @@
+import os
+from google.cloud import pubsub_v1
+
+publisher = pubsub_v1.PublisherClient()
+topic_name = "projects/zd-hackathon-2025/topics/workspace-events"
+future = publisher.publish(topic_name, b'My first message!', spam='eggs')
+try:
+    future.result()
+except Exception as e:
+    print("got exception : ", str(e))
